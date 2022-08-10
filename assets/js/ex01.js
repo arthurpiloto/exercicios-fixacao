2ba755adb1bb59d8de9b197bfa32e40a5c937463
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-10 10:27:47 -0300

    estrutura finalizada

--- assets/js/ex01.js
+++ assets/js/ex01.js
@@ -1,1 +1,27 @@
 `use strict`
+
+function diferenceResult()
+{
+    const valueA = +document.getElementById(`value-a`).value
+    const valueB = +document.getElementById(`value-b`).value
+    const result = document.getElementById(`result`)
+    const diferenceForm = document.getElementById(`diference-form`)
+
+    if (diferenceForm.reportValidity())
+    {
+        let diferenceValue = ``
+        if (valueB > valueA)
+        {
+            diferenceValue = valueB - valueA
+        }
+        else
+        {
+            diferenceValue = valueA - valueB
+        }
+
+        result.textContent = `${valueA} - ${valueB} = ${diferenceValue}`
+    }
+
+}
+
+const calculate = document.getElementById(`calculate`).addEventListener(`click`, diferenceResult)
