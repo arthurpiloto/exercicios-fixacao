a8b042de9ccc7b0d94f27b0182703b34f9e2e22a
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-15 11:14:39 -0300

    Iniciando exercício 02 (js)

--- assets/js/ex02.js
+++ assets/js/ex02.js
@@ -1,1 +1,18 @@
 `use strict`
+
+function readNumber()
+{
+    const valN = +document.getElementById(`value-n`).value
+    const result = document.getElementById(`result`)
+
+    if (valN < 0)
+    {
+        valN = valN * (-1)
+    }
+    else
+    {
+        result.textContent = `Valor positivo: ${valN}`
+    }
+}
+
+const valReader = document.getElementById(`val-reader`).addEventListener(`click`, readNumber)
