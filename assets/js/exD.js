032360d45ffd7b0c521489ddc43651350a71a8b6
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-17 10:49:45 -0300

    exercício D finalizado

--- assets/js/exD.js
+++ assets/js/exD.js
@@ -7,15 +7,45 @@ function mediaCalculate(n1, n2, n3, n4)
     const nota3 = +document.getElementById(`n3`).value
     const nota4 = +document.getElementById(`n4`).value
     const mediaForm = document.getElementById(`media-form`)
+    const result = document.getElementById(`result`)
 
     if (mediaForm.reportValidity())
     {
         if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10 || nota4 < 0 || nota4 > 10)
         {
-            return console.log(`ERROR`)
+            return alert(`ERROR`)
         }
 
-        media = (nota1 + nota2 + nota3 + nota4) / 4
-        return media.toFixed(1)
+        let media = (nota1 + nota2 + nota3 + nota4) / 4
+        let status
+
+        if (media >= 7)
+        {
+            status = `aprovado`
+        }
+        else
+        {
+            let examScore = parseFloat(prompt(`ALUNO REPROVADO. INSIRA A NOTA DO EXAME`))
+            if (examScore < 0 || examScore > 10)
+            {
+                return alert(`ERROR`)
+            }
+            else
+            {
+                let mediaExam = (examScore + media) / 2
+    
+                if (mediaExam >= 5)
+                {
+                    status = `aprovado em exame`
+                }
+                else
+                {
+                    status = `reprovado em exame`
+                }
+            }
+        }
+        result.textContent = `Média = ${media.toFixed(1)}. O aluno foi ${status}.`
     }
 }
+
+const calculateMedia = document.getElementById(`calculate`).addEventListener(`click`, mediaCalculate)
