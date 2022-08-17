40cef6131d5ca8053a4579e1f7bce40aa8b7b859
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-17 11:24:36 -0300

    exercício L iniciado

--- assets/js/exL.js
+++ assets/js/exL.js
@@ -1,1 +1,13 @@
 `use strict`
+
+function readNumber()
+{
+    const value = +document.getElementById(`value`)
+    const form = document.getElementById(`diference-form`)
+    const result = document.getElementById(`result`)
+
+    if (form.reportValidity())
+    {
+        
+    }
+}
