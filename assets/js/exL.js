0d0d761b51563d073e9fb0885760d1792c2850a8
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-18 11:07:52 -0300

    exercício L finalizado

--- assets/js/exL.js
+++ assets/js/exL.js
@@ -2,12 +2,21 @@
 
 function readNumber()
 {
-    const value = +document.getElementById(`value`)
+    const value = +document.getElementById(`value`).value
     const form = document.getElementById(`diference-form`)
     const result = document.getElementById(`result`)
 
     if (form.reportValidity())
     {
-        
+        if (value < 3)
+        {
+            result.textContent = `${value} é menor que 3`
+        }
+        else
+        {
+            result.textContent = `${value} é maior que 3`
+        }
     }
 }
+
+const numberReader = document.getElementById(`calculate`).addEventListener(`click`, readNumber)
